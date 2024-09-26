e17c6fb6b0514ae5bf4d5ec5bff125095317ac68
phidn <dangphi20102017@gmail.com>
2024-09-26 10:38:24 +0700

    first commit

---
+++ src/main/java/com/example/swp391_fall24_taxi_be/Swp391Fall24TaxiBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.swp391_fall24_taxi_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391Fall24TaxiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swp391Fall24TaxiBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swp391_fall24_taxi_be/Swp391Fall24TaxiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swp391_fall24_taxi_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391Fall24TaxiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
